2f3116a25756bede38ccbe0afc21c0221142ccfc
lehippo <groschat@sina.com>
2024-06-10 14:02:46 +0800

    changes made from theia lab environment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+// import Budget from './components/Budget.js'
+import Remaining from './components/Remaining.js'
+import ExpenseTotal from './components/ExpenseTotal.js'
+import ExpenseItem from './components/ExpenseList.js'
+import ExpenseList from './components/ExpenseList.js'
+import AllocationFrom from './components/AllocationForm.js'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -16,27 +22,39 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {
-                            /* Add Budget component here */
+                            // Budget component
+                             <div className='col-sm'>
+                             <Budget />
+                             </div>
                         }        
 
                         {
                             /* Add Remaining component here*/
+                            <div className='col-sm'>
+                            <Remaining />
+                            </div>
                         }        
 
                         {
-                            /* Add ExpenseTotal component here */
+                         //ExpenseTotal component
+                        <div className='col-sm'>
+                        <ExpenseTotal />
+                        </div>
                         }        
                        
                         {
                             /* Add ExpenseList component here */
+                            <div> <ExpenseList /> </div>
                         }         
 
                         {
                             /* Add ExpenseItem component here */
+                            <div> <ExpenseItem /> </div>
                         }        
 
                         {
                             /* Add AllocationForm component here under */
+                            <div> <AllocationFrom /> </div>
                         }        
 
                 </div>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,23 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const handleBudgetChange = (event) => {
+        if (event.target.value <20001){
+            
+            setNewBudget(event.target.value);
+        }else{
+            alert("max budget = 20,000")
+        setNewBudget(20000);}
+    }
+    return (
+<div className='alert alert-secondary'>
+<span>Budget: £{newBudget}</span>
+<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+</div>
+    );
+};
+export default Budget;
 
